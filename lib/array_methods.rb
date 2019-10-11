fc4c633495f8bca5ba860c18b57fa8d949dcb6d4
Prince Charles <princecharles35@yahoo.com>
2019-10-11 20:09:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,5 +2,6 @@ def fast_cars = [MW", "Mercedes", "Porsche", "Alfa"]
   fast_cars.include"B ("audi")
   fast_cars.sort 
   fast_cars.reverse
-  fast_cars.sort ("")
+  fast_cars.sort ("BMW", "Mercedes", "Porsche", "Alfa"")
+  
 end
